bbfa1da93801803ddae7d02ff47b2320a07c1e97
Hasibul Haque <hasibul2363@gmail.com>
2019-12-22 17:21:27 +0600

    Inbetween dev

--- src/CoolBrains.Bus.RabbitMQ/RabbitConfig.cs
+++ src/CoolBrains.Bus.RabbitMQ/RabbitConfig.cs
@@ -9,5 +9,7 @@ namespace CoolBrains.Bus.RabbitMQ
         public string ConnectionString { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
+        public string QueueName { get; set; }
+
     }
 }
---
+++ src/CoolBrains.Bus.RabbitMQ/RabbitMqBusMessageListener.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CoolBrains.Infrastructure.Extensions;
+using GreenPipes;
+using MassTransit;
+using MassTransit.RabbitMqTransport;
+using MassTransit.RabbitMqTransport.Transport;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace CoolBrains.Bus.RabbitMQ
+{
+    public static class RabbitMqBusMessageListener
+    {
+        private static RabbitConfig _rabbitConfig;
+        private static IBusControl _busControl;
+        public static IRabbitMqHost _rabbitMqHost;
+
+        private static Dictionary<string, Action<IRabbitMqReceiveEndpointConfigurator>> _queueConfigurations = new Dictionary<string, Action<IRabbitMqReceiveEndpointConfigurator>>();
+        public static ICoolBrainsServiceBuilder Listen(this ICoolBrainsServiceBuilder builder, Action<IRabbitMqReceiveEndpointConfigurator> queueConfiguration = null)
+        {
+            _rabbitConfig = builder.Services.BuildServiceProvider().GetService<IOptions<RabbitConfig>>().Value;
+            _queueConfigurations.Add(_rabbitConfig.QueueName, queueConfiguration);
+            return builder;
+        }
+
+        private static void Initialize()
+        {
+            _busControl = MassTransit.Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                _rabbitMqHost = cfg.Host(_rabbitConfig.ConnectionString, h =>
+                {
+                    h.Username(_rabbitConfig.UserName);
+                    h.Password(_rabbitConfig.Password);
+                });
+
+                foreach (var queueConfiguration in _queueConfigurations)
+                {
+                    cfg.ReceiveEndpoint(queueConfiguration.Key, queueConfiguration.Value);
+                }
+
+
+                //if (RetryCount > 0)
+                //{
+                //    cfg.UseRetry(config =>
+                //    {
+                //        config.Interval(RetryCount, TimeSpan.FromSeconds(RetryIntervalInSec));
+                //    });
+                //}
+
+                //cfg.UseSerilog();
+            });
+        }
+
+
+        public static void Start(this ICoolBrainsServiceBuilder builder)
+        {
+            Initialize();
+            _busControl.Start();
+        }
+    }
+}
