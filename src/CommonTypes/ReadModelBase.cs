d3aaf246984b47bace331521bf60dc2654ffb99c
Hasibul Haque <hasibul2363@gmail.com>
2020-01-01 15:44:22 +0600

    added common types

---
+++ src/CommonTypes/ReadModelBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CoolBrains.Infrastructure.CommonTypes
+{
+    public abstract class ReadModelBase
+    {
+        protected ReadModelBase()
+        {
+            DateCreated = DateTime.UtcNow;
+            DateUpdated = DateTime.UtcNow;
+        }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+        public Guid UpdatedBy { get; set; }
+        public Guid CreatedBy { get; set; }
+
+        public void SetDefaultForCreation(Guid userId)
+        {
+            CreatedBy = userId;
+            UpdatedBy = userId;
+        }
+
+        public void SetDefaultForUpdate(Guid userId)
+        {
+            UpdatedBy = userId;
+            DateUpdated = DateTime.UtcNow;
+        }
+    }
+}
