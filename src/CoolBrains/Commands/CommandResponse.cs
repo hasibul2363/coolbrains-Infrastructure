e2377f05cf29c5cb473ccdd7a4a44d6309446399
Hasibul Haque <hasibul2363@gmail.com>
2020-01-05 15:52:42 +0100

    made events json ignore

--- src/CoolBrains/Commands/CommandResponse.cs
+++ src/CoolBrains/Commands/CommandResponse.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
 using CoolBrains.Infrastructure.Events;
+using Newtonsoft.Json;
 
 namespace CoolBrains.Infrastructure.Commands
 {
     public class CommandResponse
     {
+        [IgnoreDataMember]
+        [JsonIgnore]
         public IEnumerable<IEvent> Events { get; set; }
         public ValidationResult ValidationResult { get; set; }
         public object Result { get; set; }
