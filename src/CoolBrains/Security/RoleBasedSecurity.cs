a2a85d5b3f89b6ac4c4817fe61e778eb689f701d
Hasibul Haque <hasibul2363@gmail.com>
2020-01-26 16:09:01 +0100

    Added RoledBasedSecurityModel

---
+++ src/CoolBrains/Security/RoleBasedSecurity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoolBrains.Infrastructure.Security
+{
+    public class RoleBasedSecurity
+    {
+        public RoleBasedSecurity()
+        {
+            IdsAllowedToRead = new List<Guid>();
+            IdsAllowedToUpdate = new List<Guid>();
+            IdsAllowedToDelete = new List<Guid>();
+
+            RolesAllowedToRead = new List<string>();
+            RolesAllowedToDelete = new List<string>();
+            RolesAllowedToUpdate = new List<string>();
+
+        }
+        public List<Guid> IdsAllowedToRead { get; set; }
+        public List<Guid> IdsAllowedToUpdate { get; set; }
+        public List<Guid> IdsAllowedToDelete { get; set; }
+        public List<string> RolesAllowedToDelete { get; set; }
+        public List<string> RolesAllowedToUpdate { get; set; }
+        public List<string> RolesAllowedToRead { get; set; }
+
+
+    }
+}
