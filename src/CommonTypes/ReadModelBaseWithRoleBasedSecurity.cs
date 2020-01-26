3cdcbd96ced8888e0ae3bff8cd3ae31d856de944
Hasibul Haque <hasibul2363@gmail.com>
2020-01-26 17:09:23 +0100

    moved RolebasedSecurity to common types

---
+++ src/CommonTypes/ReadModelBaseWithRoleBasedSecurity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoolBrains.Infrastructure.CommonTypes
+{
+    public abstract class ReadModelBaseWithRoleBasedSecurity : RoleBasedSecurity
+    {
+        protected ReadModelBaseWithRoleBasedSecurity()
+        {
+            DateCreated = DateTime.UtcNow;
+            DateUpdated = DateTime.UtcNow;
+        }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+        public Guid UpdatedBy { get; set; }
+        public Guid CreatedBy { get; set; }
+
+        public void SetDefaultForCreation(Guid userId)
+        {
+            CreatedBy = userId;
+            UpdatedBy = userId;
+        }
+
+        public void SetDefaultForUpdate(Guid userId)
+        {
+            UpdatedBy = userId;
+            DateUpdated = DateTime.UtcNow;
+        }
+    }
+}
--- src/CommonTypes/RoleBasedSecurity.cs
+++ src/CommonTypes/RoleBasedSecurity.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
-namespace CoolBrains.Infrastructure.Security
+namespace CoolBrains.Infrastructure.CommonTypes
 {
     public class RoleBasedSecurity
     {
