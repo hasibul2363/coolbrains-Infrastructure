4403abb285fc6a79ae6d097f45b3dc9d1a82b153
Hasibul Haque <hasibul2363@gmail.com>
2020-02-25 22:32:55 +0100

    Added ActionItemEnums

---
+++ src/CommonTypes/ActionItemEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoolBrains.Infrastructure.CommonTypes
+{
+    public enum ActionItemEnum
+    {
+        Create,
+        Read,
+        Update,
+        Delete,
+        Share
+    }
+}
