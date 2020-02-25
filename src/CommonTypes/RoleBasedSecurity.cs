66c63b2db7055e64d554f29a82c2f686384a45e0
Hasibul Haque <hasibul2363@gmail.com>
2020-02-25 19:57:25 +0100

    Added IdsAllowedToShare,RolesAllowedToShare,IdsAllowedToCreate

--- src/CommonTypes/RoleBasedSecurity.cs
+++ src/CommonTypes/RoleBasedSecurity.cs
@@ -7,21 +7,32 @@ namespace CoolBrains.Infrastructure.CommonTypes
     {
         public RoleBasedSecurity()
         {
+            IdsAllowedToCreate = new List<Guid>();
             IdsAllowedToRead = new List<Guid>();
             IdsAllowedToUpdate = new List<Guid>();
             IdsAllowedToDelete = new List<Guid>();
+            IdsAllowedToShare = new List<Guid>();
 
+            RolesAllowedToCreate  = new List<Guid>();
             RolesAllowedToRead = new List<string>();
             RolesAllowedToDelete = new List<string>();
             RolesAllowedToUpdate = new List<string>();
+            RolesAllowedToShare = new List<string>();
 
         }
+
+        public List<Guid> IdsAllowedToCreate { get; set; }
         public List<Guid> IdsAllowedToRead { get; set; }
         public List<Guid> IdsAllowedToUpdate { get; set; }
         public List<Guid> IdsAllowedToDelete { get; set; }
+        public List<Guid> IdsAllowedToShare { get; set; }
+
+
+        public List<Guid> RolesAllowedToCreate { get; set; }
         public List<string> RolesAllowedToDelete { get; set; }
         public List<string> RolesAllowedToUpdate { get; set; }
         public List<string> RolesAllowedToRead { get; set; }
+        public List<string> RolesAllowedToShare { get; set; }
 
 
     }
