251dabf2c10e66202267d422286db4f6464b6813
Hasibul Haque <hasibul2363@gmail.com>
2020-02-25 23:00:32 +0100

    Added Extension for Applying RoleBased security

--- src/CommonTypes/RoleBasedSecurity.cs
+++ src/CommonTypes/RoleBasedSecurity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CoolBrains.Infrastructure.CommonTypes
 {
@@ -13,7 +14,7 @@ namespace CoolBrains.Infrastructure.CommonTypes
             IdsAllowedToDelete = new List<Guid>();
             IdsAllowedToShare = new List<Guid>();
 
-            RolesAllowedToCreate  = new List<Guid>();
+            RolesAllowedToCreate = new List<string>();
             RolesAllowedToRead = new List<string>();
             RolesAllowedToDelete = new List<string>();
             RolesAllowedToUpdate = new List<string>();
@@ -28,12 +29,86 @@ namespace CoolBrains.Infrastructure.CommonTypes
         public List<Guid> IdsAllowedToShare { get; set; }
 
 
-        public List<Guid> RolesAllowedToCreate { get; set; }
+        public List<string> RolesAllowedToCreate { get; set; }
         public List<string> RolesAllowedToDelete { get; set; }
         public List<string> RolesAllowedToUpdate { get; set; }
         public List<string> RolesAllowedToRead { get; set; }
         public List<string> RolesAllowedToShare { get; set; }
 
 
+    }
+
+    public static class RoleBasedSecurityExtension
+    {
+        public static void ApplyPermission(this RoleBasedSecurity roleBasedSecurity, List<Guid> ids, List<ActionItemEnum> actionItems)
+        {
+            actionItems.ForEach(action =>
+            {
+                switch (action)
+                {
+                    case ActionItemEnum.Create:
+                        var changedItems = ids.Except(roleBasedSecurity.IdsAllowedToCreate).ToList();
+                        if (changedItems.Any())
+                            roleBasedSecurity.IdsAllowedToCreate.AddRange(changedItems);
+                        break;
+                    case ActionItemEnum.Read:
+                        changedItems = ids.Except(roleBasedSecurity.IdsAllowedToRead).ToList();
+                        if (changedItems.Any())
+                            roleBasedSecurity.IdsAllowedToRead.AddRange(changedItems);
+                        break;
+                    case ActionItemEnum.Update:
+                        changedItems = ids.Except(roleBasedSecurity.IdsAllowedToUpdate).ToList();
+                        if (changedItems.Any())
+                            roleBasedSecurity.IdsAllowedToUpdate.AddRange(changedItems);
+                        break;
+                    case ActionItemEnum.Delete:
+                        changedItems = ids.Except(roleBasedSecurity.IdsAllowedToDelete).ToList();
+                        if (changedItems.Any())
+                            roleBasedSecurity.IdsAllowedToDelete.AddRange(changedItems);
+                        break;
+                    case ActionItemEnum.Share:
+                        changedItems = ids.Except(roleBasedSecurity.IdsAllowedToShare).ToList();
+                        if (changedItems.Any())
+                            roleBasedSecurity.IdsAllowedToShare.AddRange(changedItems);
+                        break;
+                }
+            });
+        }
+
+
+        public static void ApplyPermission(this RoleBasedSecurity roleBasedSecurity, List<string> roles, List<ActionItemEnum> actionItems)
+        {
+            actionItems.ForEach(action =>
+            {
+                switch (action)
+                {
+                    case ActionItemEnum.Create:
+                        var changedRoles = roles.Except(roleBasedSecurity.RolesAllowedToCreate).ToList();
+                        if (changedRoles.Any())
+                            roleBasedSecurity.RolesAllowedToCreate.AddRange(changedRoles);
+                        break;
+                    case ActionItemEnum.Read:
+                        changedRoles = roles.Except(roleBasedSecurity.RolesAllowedToRead).ToList();
+                        if (changedRoles.Any())
+                            roleBasedSecurity.RolesAllowedToRead.AddRange(changedRoles);
+                        break;
+                    case ActionItemEnum.Update:
+                        changedRoles = roles.Except(roleBasedSecurity.RolesAllowedToUpdate).ToList();
+                        if (changedRoles.Any())
+                            roleBasedSecurity.RolesAllowedToUpdate.AddRange(changedRoles);
+                        break;
+                    case ActionItemEnum.Delete:
+                        changedRoles = roles.Except(roleBasedSecurity.RolesAllowedToDelete).ToList();
+                        if (changedRoles.Any())
+                            roleBasedSecurity.RolesAllowedToDelete.AddRange(changedRoles);
+                        break;
+                    case ActionItemEnum.Share:
+                        changedRoles = roles.Except(roleBasedSecurity.RolesAllowedToShare).ToList();
+                        if (changedRoles.Any())
+                            roleBasedSecurity.RolesAllowedToShare.AddRange(changedRoles);
+                        break;
+                }
+            });
+        }
     }
 }
