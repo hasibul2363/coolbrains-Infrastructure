2c7e6cba310e241c7a74cfc2d214820f8a3c028e
Hasibul Haque <hasibul2363@gmail.com>
2020-02-28 20:35:49 +0100

    Added method to get action items

--- src/CommonTypes/ReadModelBaseWithRoleBasedSecurity.cs
+++ src/CommonTypes/ReadModelBaseWithRoleBasedSecurity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CoolBrains.Infrastructure.CommonTypes
@@ -26,6 +27,40 @@ namespace CoolBrains.Infrastructure.CommonTypes
         {
             UpdatedBy = userId;
             DateUpdated = DateTime.UtcNow;
+        }
+
+        public List<ActionItemEnum> GetActionItems(Guid? userId, List<string> roles = null)
+        {
+            var actionItems = new List<ActionItemEnum>();
+            if (userId.HasValue)
+            {
+                if (IdsAllowedToCreate.Contains(userId.Value))
+                    actionItems.Add(ActionItemEnum.Create);
+                if (IdsAllowedToRead.Contains(userId.Value))
+                    actionItems.Add(ActionItemEnum.Read);
+                if (IdsAllowedToUpdate.Contains(userId.Value))
+                    actionItems.Add(ActionItemEnum.Update);
+                if (IdsAllowedToDelete.Contains(userId.Value))
+                    actionItems.Add(ActionItemEnum.Delete);
+                if (IdsAllowedToShare.Contains(userId.Value))
+                    actionItems.Add(ActionItemEnum.Share);
+            }
+
+            if (roles!=null)
+            {
+                if (RolesAllowedToCreate.Intersect(roles).Any())
+                    actionItems.Add(ActionItemEnum.Create);
+                if (RolesAllowedToRead.Intersect(roles).Any())
+                    actionItems.Add(ActionItemEnum.Read);
+                if (RolesAllowedToUpdate.Intersect(roles).Any())
+                    actionItems.Add(ActionItemEnum.Update);
+                if (RolesAllowedToDelete.Intersect(roles).Any())
+                    actionItems.Add(ActionItemEnum.Delete);
+                if (RolesAllowedToShare.Intersect(roles).Any())
+                    actionItems.Add(ActionItemEnum.Share);
+            }
+
+            return actionItems.Distinct().ToList();
         }
     }
 }
